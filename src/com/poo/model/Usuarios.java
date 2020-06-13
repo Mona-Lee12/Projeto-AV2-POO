93a92935f7f134720e24a02b467ee46bfdeb3346
Mona-Lee Braga <41974988+Mona-Lee12@users.noreply.github.com>
2020-06-13 19:57:17 -0300

    Create Usuarios.java

---
+++ src/com/poo/model/Usuarios.java
@@ -0,0 +1,13 @@
+package com.poo.model;
+
+public class Usuarios {
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
