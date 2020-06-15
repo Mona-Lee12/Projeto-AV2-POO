bdfeb0228e7a22cb019f485ef1ce6d363a415845
Mona-Lee Braga <41974988+Mona-Lee12@users.noreply.github.com>
2020-06-15 19:20:18 -0300

    Add files via upload

---
+++ src/com/poo/model/Categorias.java
@@ -0,0 +1,20 @@
+package com.poo.model;
+
+public class Categorias {
+	
+	private int categoriaId; 
+	private String nome;
+	
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
