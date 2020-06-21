22541d44c604035a2e574540607eb556acad5939
Mona-Lee Braga <41974988+Mona-Lee12@users.noreply.github.com>
2020-06-21 12:14:46 -0300

    ajustes na chamadas dos panels e cores

--- src/com/poo/view/MenuLateral.java
+++ src/com/poo/view/MenuLateral.java
@@ -21,8 +21,8 @@ public class MenuLateral extends JFrame {
 	private JPanel contentPane;
 	private PanelHomePage panelHomePage;
 	private PanelCategorias panelCategoriasPage;
-	private PanelArquivos panelArquivosPage;
-	private PanelUsuarios panelUsuariosPage;
+	private PanelProdutos panelProdutosPage;
+	private PanelFuncionarios panelFuncionariosPage;
 	
 	
 	public static void main(String[] args) {
@@ -43,29 +43,30 @@ public class MenuLateral extends JFrame {
 	 */
 	public MenuLateral() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 789, 390);
+		setBounds(100, 100, 924, 419);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(245, 255, 250));
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		panelHomePage = new PanelHomePage();
+		panelHomePage.setBounds(0, 0, 736, 364);
 		panelCategoriasPage = new PanelCategorias();
-		panelArquivosPage = new PanelArquivos();
-		panelUsuariosPage = new PanelUsuarios();
+		panelProdutosPage = new PanelProdutos();
+		panelFuncionariosPage = new PanelFuncionarios();
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 156, 351);
-		panel.setBackground(new Color(176, 224, 230));
+		panel.setBounds(0, 0, 156, 380);
+		panel.setBackground(new Color(153, 204, 204));
 		contentPane.add(panel);
 				panel.setLayout(null);
 			
 				
 		JLabel paneImagem = new JLabel("");
-		paneImagem.setBounds(10, 6, 136, 120);
+		paneImagem.setBounds(20, 17, 136, 120);
 		panel.add(paneImagem);
-		paneImagem.setIcon(new ImageIcon("icons\\logo.png"));
+		paneImagem.setIcon(new ImageIcon("icons\\carro.png"));
 		
 		JPanel panelHome = new JPanel();
 		panelHome.addMouseListener(new PanelButtonMouseAdapter(panelHome) {
@@ -75,11 +76,12 @@ public class MenuLateral extends JFrame {
 			}
 		});
 		panelHome.setBounds(10, 149, 136, 36);
-		panelHome.setBackground(new Color(176, 224, 230));
+		panelHome.setBackground(new Color(153, 204, 204));
 		panel.add(panelHome);
 		panelHome.setLayout(null);
 		
 		JLabel labelHome = new JLabel("Home");
+		labelHome.setBackground(new Color(153, 204, 204));
 		labelHome.setHorizontalAlignment(SwingConstants.CENTER);
 		labelHome.setFont(new Font("Segoe UI Light", Font.BOLD, 18));
 		labelHome.setBounds(6, 0, 130, 36);
@@ -89,15 +91,15 @@ public class MenuLateral extends JFrame {
 		panelUsuario.addMouseListener(new PanelButtonMouseAdapter(panelUsuario) {
 			@Override 
 			public void mouseClicked(MouseEvent e) {
-				menuClicked(panelUsuariosPage);
+				menuClicked(panelFuncionariosPage);
 			}
 		});
 		panelUsuario.setBounds(10, 184, 136, 36);
-		panelUsuario.setBackground(new Color(176, 224, 230));
+		panelUsuario.setBackground(new Color(153, 204, 204));
 		panel.add(panelUsuario);
 		panelUsuario.setLayout(null);
 		
-		JLabel lblUsuarios = new JLabel("Usu\u00E1rios");
+		JLabel lblUsuarios = new JLabel("Funcion\u00E1rios");
 		lblUsuarios.setBounds(6, 0, 130, 36);
 		lblUsuarios.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUsuarios.setFont(new Font("Segoe UI Light", Font.BOLD, 18));
@@ -107,19 +109,20 @@ public class MenuLateral extends JFrame {
 		panelArquivos.addMouseListener(new PanelButtonMouseAdapter(panelArquivos) {
 			@Override 
 			public void mouseClicked(MouseEvent e) {
-				menuClicked(panelArquivosPage);
+				menuClicked(panelProdutosPage);
 			}
 		});
 		panelArquivos.setBounds(10, 219, 136, 36);
-		panelArquivos.setBackground(new Color(176, 224, 230));
+		panelArquivos.setBackground(new Color(153, 204, 204));
 		panel.add(panelArquivos);
 		panelArquivos.setLayout(null);
 		
-		JLabel lblArquivos = new JLabel("Arquivos");
-		lblArquivos.setBounds(6, 0, 130, 36);
-		lblArquivos.setHorizontalAlignment(SwingConstants.CENTER);
-		lblArquivos.setFont(new Font("Segoe UI Light", Font.BOLD, 18));
-		panelArquivos.add(lblArquivos);
+		JLabel lblProdutos = new JLabel("Produtos");
+		lblProdutos.setBackground(new Color(153, 204, 204));
+		lblProdutos.setBounds(6, 0, 130, 36);
+		lblProdutos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProdutos.setFont(new Font("Segoe UI Light", Font.BOLD, 18));
+		panelArquivos.add(lblProdutos);
 		
 		JPanel panelCategorias = new JPanel();
 		panelCategorias.addMouseListener(new PanelButtonMouseAdapter(panelCategorias) {
@@ -129,19 +132,19 @@ public class MenuLateral extends JFrame {
 			}
 		});
 		panelCategorias.setBounds(10, 253, 136, 36);
-		panelCategorias.setBackground(new Color(176, 224, 230));
+		panelCategorias.setBackground(new Color(153, 204, 204));
 		panel.add(panelCategorias);
 		panelCategorias.setLayout(null);
 		
 		JLabel lblCateg = new JLabel("Categorias");
-		lblCateg.setBounds(6, 6, 130, 36);
+		lblCateg.setBounds(6, 0, 124, 36);
 		lblCateg.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCateg.setFont(new Font("Segoe UI Light", Font.BOLD, 18));
 		panelCategorias.add(lblCateg);	
 		
 		JButton btnNewButton = new JButton("Sair");
-		btnNewButton.setBackground(new Color(175, 238, 238));
-		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setForeground(new Color(51, 102, 102));
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -161,14 +164,14 @@ public class MenuLateral extends JFrame {
 		panel.add(btnNewButton);
 		
 		JPanel panelContentPage = new JPanel();
-		panelContentPage.setBounds(166, 11, 597, 329);
+		panelContentPage.setBounds(166, 11, 736, 363);
 		contentPane.add(panelContentPage);
 		panelContentPage.setLayout(null);
 		
 		panelContentPage.add(panelHomePage);
 		panelContentPage.add(panelCategoriasPage);
-		panelContentPage.add(panelUsuariosPage);
-		panelContentPage.add(panelArquivosPage);
+		panelContentPage.add(panelFuncionariosPage);
+		panelContentPage.add(panelProdutosPage);
 		
 		menuClicked(panelHomePage);
 	}
@@ -177,8 +180,8 @@ public class MenuLateral extends JFrame {
 	public void menuClicked(JPanel painelSelecionado) {
 		panelHomePage.setVisible(false);
 		panelCategoriasPage.setVisible(false);
-		panelUsuariosPage.setVisible(false);
-		panelArquivosPage.setVisible(false);
+		panelFuncionariosPage.setVisible(false);
+		panelProdutosPage.setVisible(false);
 		
 		painelSelecionado.setVisible(true);
 	}
@@ -195,7 +198,7 @@ public class MenuLateral extends JFrame {
 		}
 		@Override
 		public void mouseExited(MouseEvent e) {
-			panel.setBackground(new Color(176,224,230));
+			panel.setBackground(new Color(153, 204, 204));
 		}
 		@Override
 		public void mousePressed(MouseEvent e) {
@@ -203,7 +206,7 @@ public class MenuLateral extends JFrame {
 		}
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			panel.setBackground(new Color(176,224,230));
+			panel.setBackground(new Color(153, 204, 204));
 		}		
 	}
 }
